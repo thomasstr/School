f45bdf17ea73e32b735448b399267621c1e97b86
Thomas Stroemme <strtho@gmail.com>
2012-10-17 15:12:16 +0200

    added new and compiled code

--- pg3400-12/f7/string_ex_1.c
+++ pg3400-12/f7/string_ex_1.c
@@ -3,15 +3,26 @@
 
 int main(int argc, char* argv[])
 {
-	printf("%s\n", argv[0]);
+	/*printf("%s\n", argv[0]); */
 	char hei[] = "hei!";
-	
 	printf("Stringen sier: \"%s\".\n", hei);
 	
 	for(int i = 0; i < MAX; ++i)
 	{
-		printf("    %c     ", hei);
+		printf("   %c ", hei[i]);
+
 	}
+	
+	printf("\n");
+	
+	for(int i = 0; i < MAX; ++i)
+	{
+		printf("0x%02x ", hei[i]);
+
+	}
+	
+	printf("\n");
+	
 	return 0;
 	
 }
